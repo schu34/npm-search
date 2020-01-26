5f1ce211a2730fbf7bf6b6cc8a5ab2b431a85342
Matthew Schupack <schu34@gmail.com>
2020-01-26 16:13:20 -0500

    remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-var getPackageReadme = require('get-package-readme')
-
-getPackageReadme('express', function (err, readme) {
-  if (err) throw err
-  console.log(readme
-    .replace(/<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[\^'">\s]+))?)+\s*|\s*)\/?>/gms, "")
-    .replace(/(\n)+/, "\n"))
-})
