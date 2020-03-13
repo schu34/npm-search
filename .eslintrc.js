a2a8e7872452f4645eb2d6df6950790a495d9f4d
Matthew Schupack <schu34@gmail.com>
2020-03-13 19:55:22 -0400

    lint tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,26 @@
 module.exports = {
-	'env': {
-		'es6': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
-		},
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			2,
-			{ 'SwitchCase': 1 }
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'double'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  env: {
+    es6: true,
+		node: true,
+		// "jest/global":true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jest/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
