0cca3554e64338b4ebe4ad75351f8f4bc442ebdc
Matthew Schupack <schu34@gmail.com>
2020-03-21 19:01:44 -0400

    make titles bold

--- __tests__/Markdown.test.jsx
+++ __tests__/Markdown.test.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import fs from "fs";
-import Markdown from "../src/Markdown";
+import Markdown, { stripHashes } from "../src/Markdown";
 import { render } from "ink-testing-library";
+
+
+// This is required to normalize snapshot testing across 
+// different terminals. It's not ideal since we can't test
+// to make sure styling is happening correctly.
+
+import Chalk from "chalk";
+Chalk.level = 0;
 
 const mdString = fs.readFileSync("./readme.md").toString();
 
@@ -11,6 +19,18 @@ describe("<Markdown />", () => {
       <Markdown source={mdString} width={50} height={100} />
     );
     rerender(<Markdown source={mdString} width={50} height={100} />);
-    expect(lastFrame()).toMatchSnapshot();
+  expect(lastFrame()).toMatchSnapshot();
+  });
+});
+
+describe("stripHashes", () => {
+  it("should strip one hash", () => {
+    expect(stripHashes("#hello")).toBe("hello");
+  });
+  it("should strip many hashes", () => {
+    expect(stripHashes("#####hello")).toBe("hello");
+  });
+  it("should preserve spaces in the string", () => {
+    expect(stripHashes("###hello hello")).toBe("hello hello");
   });
 });
